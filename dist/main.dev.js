3c0ec4c4789e134e12a90e3b64d9bfbf8c5bb412
Glebka90 <glebentinko2@gmail.com>
2023-05-04 11:11:05 +0300

    first commit

---
+++ dist/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+  $(".slick-vertical").slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    arrows: true,
+    dots: true
+  });
+});
+$(".slider").on("afterChange", function (event, slick, currentSlide) {
+  $(".count").text("0" + parseInt(slick.currentSlide + 1));
+});
